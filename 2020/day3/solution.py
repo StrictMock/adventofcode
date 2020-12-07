4521f044ddb12924d8189c3f56e8b7425e41a952
StrictMock <darekg6@gmail.com>
2020-12-07 21:06:03 +0100

    Add solution day 3 part 2 - Python

--- 2020/day3/solution.py
+++ 2020/day3/solution.py
@@ -6,19 +6,35 @@ def read_input():
         return [x.rstrip() for x in fin.readlines()]
 
 
-def part_one(data):
-    x = 0
+def count_trees(data, step_right, step_down):
+    col = 0
     counter = 0
-    for y in data:
-        if '#' in y[x % len(y)]:
+    for y in range(0, len(data), step_down):
+        row = data[y]
+        if '#' in row[col % len(row)]:
             counter = counter + 1
-        x = x + 3
+        col = col + step_right
+    return counter
 
+
+def part_one(data):
+    counter = count_trees(data=data, step_right=3, step_down=1)
     print("part one: {}".format(counter))
 
 
 def part_two(data):
-    pass
+    steps = [
+        {"step_right": 1, "step_down": 1},
+        {"step_right": 3, "step_down": 1},
+        {"step_right": 5, "step_down": 1},
+        {"step_right": 7, "step_down": 1},
+        {"step_right": 1, "step_down": 2}]
+
+    result = 1
+    for step in steps:
+        result = result * \
+            count_trees(data, step["step_right"], step["step_down"])
+    print("part two: {}".format(result))
 
 
 def main():
